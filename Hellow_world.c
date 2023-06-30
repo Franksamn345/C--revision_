53722a68e6daa6a82d54c4005599b1e8d2b0acc3
Franksamn345 <franksamntos@gmail.com>
2023-06-30 20:26:02 +0300

    Hellow world

---
+++ Hellow_world.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main- Entry to the code
+ *
+ *Return: 0 (successfull)
+
+ */
+int main(void)
+{
+	char sam[100] = "Hello world of coders\nI'm Frank Samn, I as the founder would like to welcome u all";
+	printf("%s", sam);
+
+	return (0);
+}
+
---
+++ README.md
@@ -0,0 +1,4 @@
+# My C revisions on different topics
+## Main objective is to understand what is C and how to use it well
+* Make different examples about the language 
+* Do different projects each day inorder to make a difference
