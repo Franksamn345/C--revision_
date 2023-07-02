2ba5e53e2f3d3d1f7197a644727e244452480910
Franksamn345 <franksamntos@gmail.com>
2023-07-02 14:42:20 +0300

    Grading

--- grading.c
+++ grading.c
@@ -14,34 +14,55 @@
 int main(void)
 
 {
-	int score = 70;
+	int score = 30;
 
         /** Check if the score is between 
 	 * 80 and 100 print the grade if true
 	 */
 	if (score >= 80 && score <= 100)
+	{
 		printf("A");
+	}
 
 	/** Check if the score is between
 	 * 70 and 79 print the grade if true
 	 */
 	else if (score >= 70 && score <= 79)
+	{
 		printf("B");
+	}
 	/** Check if the score is between
 	 * 65 and 69 print the grade if true
 	 */
 	else if (score >= 65 && score <= 69)
+	{
 		printf("C");
+	}
 	/** Check if the score is between
 	 * 60 and 64 print the grade if true
 	 */
 	else if (score >= 60 && score <= 64)
+	{
 		printf("D");
+	}
 	/** Check if the score is between
 	 * 50 and 59 print the grade if true
 	 */
 	else if (score >= 50 && score <= 59)
+	{
 		printf("D");
+	}
+	/** Check if the score is between
+	 * 50 and 59 print the grade if true
+	 */
+	else if (score >= 0 && score <= 49)
+	{
+		printf("E");
+	}
+	else
+	{
+		printf("You enterd an invalid number");
+	}
         
 	return (0);
 }
